f7c2f00f84ce4b0581a180a2f11d7021825f89ea
neerajgupta2407 <neerajgupta2407@gmail.com>
2015-07-06 16:46:19 +0530

    app4: in opogress

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.core.urlresolvers import reverse
 #from  django.template import RequestContext, loader
-from .models import Question
+from .models import Question, Choice
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
@@ -34,8 +35,19 @@ def detail(request, question_id):
 	
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    #response = "You're looking at the results of question %s."
+    #return HttpResponse(response % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/results.html' ,{'question' : question})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)	
+	p = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice  = p.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist)	:
+		return render(request, 'polls/details.html', {'question' : p, 'error_message' : "You didn't select a choice.",})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		return HttpResponseRedirect(reverse('polls:results',args=(p.id,)))
+    #return HttpResponse("You're voting on question %s." % question_id)	
