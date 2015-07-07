5fcdd92eed18abe48ef567936a0f4298eff8cab2
neerajgupta2407 <neerajgupta2407@gmail.com>
2015-07-07 14:53:56 +0530

    app part 5: in progress

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,4 @@
+import datetime
 from django.db import models
 from django.utils import timezone
 
@@ -10,7 +11,9 @@ class Question(models.Model):
 		return self.question_text
 		
 	def was_published_recently(self):
-		return self.pub_date >= timezone.now() - timezone.timedelta(days=1)
+		now  = timezone.now()
+		#return self.pub_date >= timezone.now() - timezone.timedelta(days=1)
+		return now - datetime.timedelta(days=1) <= self.pub_date <= now
 	was_published_recently.admin_order_field = "pub_date"	
 	was_published_recently.boolean = True
 	was_published_recently.short_description = 'Published Recently'
